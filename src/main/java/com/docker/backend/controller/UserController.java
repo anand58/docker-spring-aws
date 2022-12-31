edddd05279441a9d0f6f958b31fa8072d806f250
anand58 <anandt.tiwari12@gmail.com>
2022-12-31 12:06:20 +0530

    tested

--- src/main/java/com/docker/backend/controller/UserController.java
+++ src/main/java/com/docker/backend/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.docker.backend.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api")
@@ -11,6 +15,11 @@ public class UserController {
     @GetMapping("user")
     public String getUser() {
         return "Anand Is Tested";
+    }
+
+    @GetMapping("webhook")
+    public void executeWebHook() throws IOException {
+        Runtime.getRuntime().exec(new String[]{"sh", "./src/redeploy.sh"});
     }
     
 }
